7ba69602f6e01c5c2ffb310d985fcd73e769e454
xufangjie <xufangjie2008@163.com>
2022-02-18 16:43:59 +0800

    update

--- src/main/java/com/mingshashan/learn/design/pattern/SingleResponsibilityPrinciple.java
+++ src/main/java/com/mingshashan/learn/design/pattern/SingleResponsibilityPrinciple.java
@@ -1,4 +1,34 @@
 package com.mingshashan.learn.design.pattern;
 
 public class SingleResponsibilityPrinciple {
+
+    interface IEmail {
+        public void setSender(String sender);
+
+        public void setReceiver(String receiver);
+
+        public void setContent(IContent content);
+    }
+
+    interface IContent {
+
+    }
+
+    class Email implements IEmail {
+        // set sender;
+        public void setSender(String sender) {
+        }
+
+        // set receiver;
+        public void setReceiver(String receiver) {
+        }
+
+        // set content;
+        public void setContent(IContent content) {
+        }
+    }
+
+    class Content implements IContent {
+
+    }
 }
